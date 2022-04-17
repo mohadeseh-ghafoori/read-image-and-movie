8711e3361f67437f1e8b7cbb1b21f8de3dd84d63
mohadeseh <m.ghafoori78@gmail.com>
2022-04-17 21:50:04 +0430

    resize video

--- basic_codes.py
+++ basic_codes.py
@@ -3,18 +3,21 @@ import cv2 as cv
 img=cv.imread("G:\learn opencv/dollar.tif")
 cv.imshow("original image", img)
 capture=cv.VideoCapture("G:\learn opencv/LEARN_OPENCV_in_3_HOURS_with_Python_Including_3xProject_WQeoO7MI0Bs_247.mkv")
-while True:
-    isTrue,frame=capture.read() #read each frame in capture and if it reads successfuly, isTrue will be True
-    cv.imshow("video",frame)
-    if cv.waitKey(25) & 0xFF == ord("q"): #after 25ms if user presses the letter q, the movie will finish 
-        break
-
-capture.release()
-cv.destroyAllWindows()
-cv.waitKey(0)
 
 #resclae video 
 def rescale_frame(frame,scale=0.5):
     width,height=frame.shape[:2]
     dimension=(int(width),int(height))
     return cv.resize(frame,dimension,interpolation=cv.INTER_LINEAR)
+
+while True:
+    isTrue,frame=capture.read() #read each frame in capture and if it reads successfuly, isTrue will be True
+    resized_frame=rescale_frame(frame,scale=0.5)
+    cv.imshow("original video",frame)
+    cv.imshow("resized video",resized_frame)
+    if cv.waitKey(25) & 0xFF == ord("q"): #after 25ms if user presses the letter q, the movie will finish 
+        break
+    
+capture.release()
+cv.destroyAllWindows()
+cv.waitKey(0)
